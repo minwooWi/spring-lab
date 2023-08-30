7464aebced5a791f818d723d86db8f87fb06a1fa
Minwoo Chase <c5287@nate.com>
2023-08-30 14:29:46 +0900

    Initial commit

---
+++ src/main/java/com/example/springlab/controller/testController.java
@@ -0,0 +1,12 @@
+package com.example.springlab.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+    @GetMapping
+    public String test(){
+        return "Hello World";
+    }
+}
---
+++ src/main/java/com/example/springlab/study/BeanViewer.java
@@ -0,0 +1,26 @@
+package com.example.springlab.study;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+class BeanViewer {
+
+    private final Logger LOG = LoggerFactory.getLogger(getClass());
+
+    @EventListener
+    public void showBeansRegistered(ApplicationReadyEvent event) {
+        String[] beanNames = event.getApplicationContext()
+                .getBeanDefinitionNames();
+
+        int index=0;
+
+        for(String beanName: beanNames) {
+            LOG.info("START INDEX"+index+" {}", beanName);
+            index++;
+        }
+    }
+}
