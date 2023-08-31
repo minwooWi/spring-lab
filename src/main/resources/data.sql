a39244ad81e8035e2fa5cd9534d0de2fdc751a2c
Minwoo Chase <c5287@nate.com>
2023-08-31 15:01:26 +0900

    main branch 테스트코드 제거

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-insert into member (id, name) values (1, 'name 1');
-insert into member (id, name) values (2, 'name 2');
-insert into member (id, name) values (3, 'name 3');
-insert into member (id, name) values (4, 'name 4');
